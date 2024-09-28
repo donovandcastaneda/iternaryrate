b95618c1496eadbbf3843af5544bfe8b53356fb6
donovancastaneda <donovandcastanedajr@gmail.com>
2024-09-27 19:06:10 -0500

    first commit

---
+++ Backend/src/main/java/cc/iternaryrate/iternaryrate/IternaryrateApplication.java
@@ -0,0 +1,13 @@
+package cc.iternaryrate.iternaryrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IternaryrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IternaryrateApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/cc/iternaryrate/iternaryrate/IternaryrateApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.iternaryrate.iternaryrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IternaryrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
